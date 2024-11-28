842346b41e59b111e2ea3c10da86cfec0eaf364c
Josh W <1755797+joshwyant@users.noreply.github.com>
2024-11-28 14:36:03 -0800

    Making the script more customizable and friendly

--- ubuntu/bootstrap-ubuntu.sh
+++ ubuntu/bootstrap-ubuntu.sh
@@ -1,16 +1,90 @@
 #!/bin/bash
+set -e
 
 # Personal info
+INTERACTIVE=0
+MY_USER="josh"
 GH_USERID="joshwyant"
 GH_EMAIL="1755797+joshwyant@users.noreply.github.com"
 GIT_NAME="Josh W"
 KEY_FILE=$HOME/Downloads/"Josh W (F31AE17F) – Secret.asc"
 TRUST_FILE=$HOME/Downloads/otrust.lst
 
+# Main packages
+EXTRA_PACKAGES=""
+DEV_PACKAGES="wget curl git build-essential tmux vim protobuf-compiler"
+
+# What else to install
+INSTALL_DOCKER=1
+INSTALL_GUI_APPS=1
+INSTALL_OPENSSH_SERVER=1
+INSTALL_SGX_SDK=1
+INSTALL_GRAMINE=1
+INSTALL_FORTANIX_EDP=1
+INSTALL_RUST=1
+INSTALL_PYTHON=1
+ENABLE_GIT_GPG_COMMIT_SIGNING=1
+ALLOW_OPENSSH_PASSWORD_AUTH=0
+ALLOW_OPENSSH_PUBKEY_AUTH=1
+RUST_CHANNEL="nightly"
+
+PREREQUISITES=( curl )
+NEED_RESTART=0
+
+if [[ $USER != $MY_USER ]]; then
+  INTERACTIVE=1
+  echo "This script is configured for $MY_USER. However, you are logged in as $USER, so you will be prompted for your own info interactively. Alternatively, you can customize the defaults in bootstrap-ubuntu.sh."
+
+  read -r -p "Do you want to continue and enter your own info interactively? [y/N] " yesno
+  if [[ ! (${yesno,,} =~ ^(y|yes)$) ]]; then
+    exit
+  fi
+
+  MY_USER=$USER
+  GH_USERID=$USER
+  GH_EMAIL="$USER@gmail.com"
+  GIT_NAME=$USER
+  KEY_FILE=$HOME/Downloads/Secret.asc
+  TRUST_FILE=$HOME/Downloads/otrust.lst
+fi
+
+# Directory the script is located in
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${BASEDIR}"
 
-# Install package sources
+# Windows Subsystem for Linux
+# Don't install GUI apps on WSL
+if uname -r | grep -qi "microsoft"; then
+  IS_WSL=1
+  INSTALL_GUI_APPS=0
+fi
+
+# Ubuntu release
+RELEASE="$(lsb_release -sc)"
+
+# Figure out release-specific requirements such as python version
+if [[ $RELEASE == "noble" ]]; then
+  # Ubuntu 24.04
+  PYTHON_VER="python3.12"
+elif [[ $RELEASE == "jammy" ]]; then
+  # Ubuntu 22.04
+  PYTHON_VER="python3.10"
+else
+  echo "Unsupported release $RELEASE!"
+  exit
+fi
+
+# Start building the packages list
+PACKAGES="$EXTRA_PACKAGES $DEV_PACKAGES"
+
+# Install prerequisites for this script
+for pkg in ${PREREQUISITES[@]}; do
+  if [[ -z $(apt -qq list $pkg 2>/dev/null) ]]; then
+    sudo apt install -y $pkg
+  fi
+done
+
+# For installing package sources
 add_package_source() {
   keyurl=$1
   pkg_src=$2
@@ -26,150 +100,241 @@ add_package_source() {
   fi
 }
 
+# Python and venv; also prerequisites for gramine dev
+if [[ $INSTALL_PYTHON == 1 || $INSTALL_GRAMINE == 1 ]]; then
+  PACKAGES="$PACKAGES $PYTHON_VER-venv"
+  INSTALLING_PYTHON=1
+fi
+
 # Docker
-add_package_source \
-  https://download.docker.com/linux/ubuntu/gpg \
-  https://download.docker.com/linux/ubuntu $(lsb_release -sc) stable \
-  docker.asc \
-  docker.list
+if [[ $INSTALL_DOCKER == 1 && -z $(which docker) ]]; then
+  PACKAGES="$PACKAGES docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
+
+  add_package_source \
+    https://download.docker.com/linux/ubuntu/gpg \
+    https://download.docker.com/linux/ubuntu $(lsb_release -sc) stable \
+    docker.asc \
+    docker.list
+
+  INSTALLING_DOCKER=1
+  NEED_RESTART=1
+fi
+
+# SGX; also a prerequisite for Gramine
+if [[ ($INSTALL_SGX == 1 || $INSTALL_GRAMINE == 1) && ! -f /etc/sgx_default_qcnl.conf ]]; then
+  PACKAGES="$PACKAGES
+    cpuid libsgx-quote-ex libsgx-dcap-ql
+    libsgx-dcap-quote-verify-dev libsgx-dcap-default-qpl-dev"
+
+  add_package_source \
+    https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key \
+    https://download.01.org/intel-sgx/sgx_repo/ubuntu $(lsb_release -sc) main \
+    intel-sgx-deb.asc \
+    intel-sgx.list
+
+  INSTALLING_SGX=1
+  NEED_RESTART=1
+fi
 
 # Gramine
-add_package_source \
-  https://packages.gramineproject.io/gramine-keyring-$(lsb_release -sc).gpg \
-  https://packages.gramineproject.io $(lsb_release -sc) main \
-  gramine-keyring-$(lsb_release -sc).gpg \
-  gramine.list
+if [[ $INSTALL_GRAMINE == 1 && ! $(apt -qq list gramine 2>/dev/null) ]]; then
+  PACKAGES="$PACKAGES gramine"
 
-# Intel SGX
-add_package_source \
-  https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key \
-  https://download.01.org/intel-sgx/sgx_repo/ubuntu $(lsb_release -sc) main \
-  intel-sgx-deb.asc \
-  intel-sgx.list
+  add_package_source \
+    https://packages.gramineproject.io/gramine-keyring-$(lsb_release -sc).gpg \
+    https://packages.gramineproject.io $(lsb_release -sc) main \
+    gramine-keyring-$(lsb_release -sc).gpg \
+    gramine.list
 
-# Don't install these on WSL
-if ! uname -r | grep -qi "microsoft"; then
-  # VSCode
+  INSTALLING_GRAMINE=1
+fi
+
+# Install these GUI applications unless on WSL
+# VSCode
+if [[ $INSTALL_GUI_APPS == 1 && -z $(which code) ]]; then
+  PACKAGES="$PACKAGES code"
+
   add_package_source \
     https://packages.microsoft.com/keys/microsoft.asc \
     https://packages.microsoft.com/repos/code stable main \
     packages.microsoft.asc \
     vscode.list
+  INSTALLING_CODE=1
+fi
+# Brave Browser
+if [[ $INSTALL_GUI_APPS == 1 && -z $(which brave-browser) ]]; then
+  PACKAGES="$PACKAGES brave-browser"
 
-  # Brave Browser
   add_package_source \
     https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg \
     https://brave-browser-apt-release.s3.brave.com/ stable main \
     brave-browser-archive-keyring.gpg \
     brave-browser-release.list
+  INSTALLING_BRAVE=1
 fi
 
-# Apply new package lists, and update software
-sudo apt update && sudo apt upgrade -y
-
-# Figure out python version
-if [[ $(lsb_release -sc) == "noble" ]]; then
-  # Ubuntu 24.04
-  PYTHON_VER=python3.12
-elif [[ $(lsb_release -sc) == "jammy" ]]; then
-  # Ubuntu 22.04
-  PYTHON_VER=python3.10
-else
-  echo "Unsupported release $(lsb_release -sc)!"
-  exit
+if [[ $INSTALL_OPENSSH_SERVER == 1 && -z $(which sshd) ]]; then
+  PACKAGES="$PACKAGES openssh-server"
+  INSTALLING_OPENSSH_SERVER=1
 fi
 
-# Install software
-sudo apt install -y \
-  git build-essential tmux vim \
-  $PYTHON_VER-venv \
-  cpuid \
-  openssh-server \
-  docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
-  libsgx-quote-ex libsgx-dcap-ql \
-  libsgx-dcap-quote-verify-dev libsgx-dcap-default-qpl-dev \
-  gramine \
-  pkg-config libssl-dev protobuf-compiler
+# Rust is required by Fortanix EDP
+if [[ ($INSTALL_RUST == 1 || $INSTALL_FORTANIX_EDP == 1) && -z $(which rustc) ]]; then
+  INSTALLING_RUST=1
+fi
 
-# Additional GUI applications if not WSL
-if uname -r | grep -qi "microsoft"; then
-  echo "Running under WSL"
+if [[ $INSTALL_FORTANIX_EDP == 1 && -z $(which sgx-detect) ]]; then
+  PACKAGES="$PACKAGES pkg-config libssl-dev protobuf-compiler"
+  INSTALLING_FORTANIX_EDP=1
+  RUST_CHANNEL="nightly"
+fi
+
+# Only install packages that are not already installed.
+PACKAGES_TO_INSTALL=""
+IFS=';' read -ra pkgs <<< "$IN"
+for pkg in "${pkgs[@]}"; do
+  if [[ ! $(apt -qq list $pkg 2>/dev/null) ]]; then
+    PACKAGES_TO_INSTALL=$PACKAGES_TO_INSTALL $pkg
+  fi
+done
+
+if [[ -z $PACKAGES_TO_INSTALL ]]; then
+  echo "All apt packages are already installed."
 else
-  sudo apt install -y \
-    code brave-browser
+  # Apply new package lists, and update software
+  sudo apt update && sudo apt upgrade -y
+  # Install software
+  sudo apt install -y $PACKAGES_TO_INSTALL
 fi
 
 # Configure docker
-sudo usermod -aG docker $USER
+if [[ $INSTALLING_DOCKER == 1 ]]; then
+  sudo usermod -aG docker $USER
+fi
 
 # Configure SSH
 if [[ ! -f $HOME/.ssh/id_ed25519 ]]; then
   ssh-keygen -t ed25519 -q -f "$HOME/.ssh/id_ed25519" -N "" -C "${GH_EMAIL}"
-  eval "$(ssh-agent -s)"
-  ssh-import-id-gh $GH_USERID
   cat ~/.ssh/id_ed25519.pub
-  read -n 1 -p "Save the SSH public key in GitHub and in remote authorized_keys files, and press any key to continue"
+  if [[ $INTERACTIVE == 1 ]]; then
+    read -n 1 -p "Save the SSH public key in GitHub and in remote authorized_keys files, and press any key to continue"
+  fi
+
+  eval "$(ssh-agent -s)"
 fi
 
-# Configure git
+# Set OpenSSH to start at boot
+if [[ $INSTALLING_OPENSSH_SERVER == 1 ]]; then
+  sudo systemctl enable ssh
+fi
+
+openssh_option() {
+  # Uncomment the option
+  sudo sed -s -i "s/#$1/$1/" /etc/ssh/sshd_config
+
+  # Change from no to yes or vice-versa
+  old_val="yes"
+  new_val="no"
+  if [[ $2 && $2 != "no" ]]; then
+    old_val="no"
+    new_val="yes"
+  fi
+
+  # Check if setting is incorrect, and update it
+  if [[ $(cat /etc/ssh/sshd_config | grep -i "$1 $old_val") ]]; then
+    sudo sed -s -i "s/$1 $old_val/$1 $new_val/" /etc/ssh/sshd_config
+    echo 1  # setting was changed
+  else
+    echo 0
+  fi
+}
+
+# Update on every run
+if [[ $INSTALL_OPENSSH_SERVER == 1 ]]; then
+  pass_auth_changed=$(openssh_option PasswordAuthentication $ALLOW_OPENSSH_PASSWORD_AUTH)
+  key_auth_changed=$(openssh_option PubkeyAuthentication $ALLOW_OPENSSH_PUBKEY_AUTH)
+  # Restart ssh server if any settings changed
+  if [[ $pass_auth_changed == 1 || $key_auth_changed == 1 ]]; then
+    sudo systemctl try-reload-or-restart ssh
+  fi
+  ssh-import-id-gh $GH_USERID 2>/dev/null
+fi
+
+# Configure git; update on every run
 git config --global user.email "${GH_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 
-# Configure GPG & git signing
-export KEY_FILE && export TRUST_FILE && \
+# Configure GPG & git signing; update on every run
+export KEY_FILE TRUST_FILE INTERACTIVE ENABLE_GIT_GPG_COMMIT_SIGNING && \
   ./import-gpg-key.sh
 
-# Install rust
-if [[ -z $(which rustc) ]]; then
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s --   -y --default-toolchain nightly
+# Install rust; prerequisite for Fortanix EDP
+if [[ $INSTALLING_RUST == 1 ]]; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s --   -y --default-toolchain $RUST_CHANNEL
   source "$HOME/.cargo/env"
-else
-  echo Rust is already installed.
 fi
 
 # Configure SGX
-sudo usermod -aG sgx $USER
-sudo sed -s -i 's/localhost:8081/api.trustedservices.intel.com/' /etc/sgx_default_qcnl.conf
-if [[ ! -f $HOME/.config/gramine/enclave-key.pem ]]; then
-  gramine-sgx-gen-private-key
+if [[ $INSTALLING_SGX == 1 ]]; then
+  sudo usermod -aG sgx $USER
+  sudo sed -s -i 's/localhost:8081/api.trustedservices.intel.com/' /etc/sgx_default_qcnl.conf
+fi
+
+# On every run
+if [[ $INSTALLING_GRAMINE == 1 ]]; then
+  if [[ ! -f $HOME/.config/gramine/enclave-key.pem ]]; then
+    gramine-sgx-gen-private-key
+  fi
 fi
 
 # Install Fortanix EDP
-if [[ -z $(which sgx-detect) ]]; then
+if [[ $INSTALLING_FORTANIX_EDP == 1 ]]; then
+  # Nightly toolchain is required to be the default to install Fortanix EDP
+  if [[ ! $(rustup toolchain list | grep 'nightly.*default') ]]; then
+    rustup default nightly
+  fi
   rustup target add x86_64-fortanix-unknown-sgx --toolchain nightly
   cargo install fortanix-sgx-tools sgxs-tools
-  # sgx-detect
-else
-  echo Fortanix EDP is already installed.
+  sgx-detect
 fi
 
 # Set up source repos
 mkdir -p $HOME/src
 
-read -r -p "Do you want to reboot now? [y/N] " yesno
-if [[ ${yesno,,} =~ ^(y|yes)$ ]]; then
-    sudo reboot
-fi
-
 sync() {
   # Link dotfiles
-  if [[ ! -f ~/.vimrc ]]; then
-    ln -s ./.vimrc ~/.vimrc
+  if [[ ! -f $2/$1 ]]; then
+    echo "Invalid filename $2/$1"
+    exit 1
+  fi
+  if [[ ! -f $3/$1 ]]; then
+    mkdir -p $3
+    ln -s $(readlink -f $2/$1) $3/$1
   fi
 }
 
 # Link dotfiles
-if [[ ! -f ~/.vimrc ]]; then
-  ln -s $(readlink -f ../.vimrc) ~/.vimrc
-fi
-if [[ ! -f ~/.tmux.conf ]]; then
-  ln -s $(readlink -f ../.tmux.conf) ~/.tmux.conf
+sync .vimrc .. ~
+sync .tmux.conf .. ~
+
+# Only on WSL for VSCODE: `public key decryption failed: Inappropriate ioctl for device`
+# This fix still fails (I think `public key decryption failed: Invalid IPC response`)
+if [[ $IS_WSL == 1 ]] then
+  sync gpg.conf ../WSL/.gnupg ~/.gnupg
+  sync gpg-agent.conf ../WSL/.gnupg ~/.gnupg
+  echo RELOADAGENT | gpg-connect-agent
 fi
 
-mkdir -p ~/.gnupg
-if [[ ! -f ~/.gnupg/gpg.conf ]]; then
-  ln -s $(readlink -f ../.gnupg/gpg.conf) ~/.gnupg/gpg.conf
+if [[ $(git status -s) ]]; then
+  echo ">> There are changes to the dotfiles. Please sync them in git!"
 fi
-if [[ ! -f ~/.gnupg/gpg-agent.conf ]]; then
-  ln -s $(readlink -f ../.gnupg/gpg-agent.conf) ~/.gnupg/gpg-agent.conf
+
+if [[ $NEED_RESTART == 1 ]]; then
+  echo ">> Reboot is required."
+  if [[ $INTERACTIVE == 1 ]]; then
+    read -r -p "Do you want to reboot now? [y/N] " yesno
+    if [[ ${yesno,,} =~ ^(y|yes)$ ]]; then
+        sudo reboot
+    fi
+  fi
 fi
--- ubuntu/import-gpg-key.sh
+++ ubuntu/import-gpg-key.sh
@@ -1,38 +1,50 @@
 #!/bin/bash
+set -e
 
 GH_EMAIL=$(git config --global user.email)
-if [[ -z GH_EMAIL ]]; then
+if [[ -z $GH_EMAIL ]]; then
     echo "Must set \"git config --global user.email <email>\". run import-gpg-key.sh in dotfiles again later."
     return 1
 fi
 
-read -r -p "Do you want to import your gpg key now? [Y/n] " yesno
-if [[ -n ${yesno} && ! ${yesno,,} =~ ^(y|yes)$ ]]; then
-    exit
+IMPORTED=$(gpg --list-keys | grep $GH_EMAIL)
+
+if [[ ! $IMPORTED ]]; then
+    if [[ $INTERACTIVE != "0" ]]; then
+        read -r -p "Do you want to import your gpg key now? [Y/n] " yesno
+        if [[ -n ${yesno} && ! ${yesno,,} =~ ^(y|yes)$ ]]; then
+            exit
+        fi
+
+        read -p "File name for import [${KEY_FILE}]: " key_file
+        key_file=${key_file:-"${KEY_FILE}"}
+        if [[ -z $key_file || ! -f $key_file ]]; then
+            echo Invalid filename $key_file
+            return 1
+        fi
+
+        read -p "Owner trust file name for import [${TRUST_FILE}]: " trust_file
+        trust_file=${trust_file:-"${TRUST_FILE}"}
+        if [[ -z $trust_file || ! -f $trust_file ]]; then
+            echo Invalid filename $trust_file
+            return 1
+        fi
+    fi
 fi
-
-read -p "File name for import [${KEY_FILE}]: " key_file
-key_file=${key_file:-"${KEY_FILE}"}
-
-if [[ -z $key_file || ! -f $key_file ]]; then
-    echo Invalid filename $key_file
-    return 1
-fi
-
-# Import the key
-gpg --import "${key_file}"
 
 # Update git config
-git config --global user.signingkey "${GH_EMAIL}"
-git config --global commit.gpgsign true
+if [[ $ENABLE_GIT_GPG_COMMIT_SIGNING ]]; then
+    if [[ ! $IMPORTED ]]; then
+        # Import the key
+        gpg --import "${key_file}"
 
-read -p "Owner trust file name for import [${TRUST_FILE}]: " trust_file
-trust_file=${trust_file:-"${TRUST_FILE}"}
+        # Import trust file
+        gpg --import-ownertrust "${trust_file}"
+    fi
 
-if [[ -z $trust_file || ! -f $trust_file ]]; then
-    echo Invalid filename $trust_file
-    return 1
+    git config --global user.signingkey "${GH_EMAIL}"
+    git config --global commit.gpgsign true
+else
+    git config --global --unset user.signingkey
+    git config --global --unset commit.gpgsign
 fi
-
-# Import trust file
-gpg --import-ownertrust "${trust_file}"
